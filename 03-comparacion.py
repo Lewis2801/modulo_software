6eefb1a17692cb5c7184f45340f5c2703576dd17
Lewis2801 <levialexandermartinezrod14@gmail.com>
2024-08-19 14:18:17 -0600

    Inicio módulo
    Se agregan 4 archivos al repositorio

---
+++ 03-comparacion.py
@@ -0,0 +1,17 @@
+# Operadores de comparación
+
+num1 = 10;
+num2 = 2.5;
+Texto = "Hola";
+
+# Mayor que
+print(num1 > num2);
+
+# Menor que
+print(num1 < num2);
+
+# Menor o igual que
+print(num1 <= num2);
+
+# Mayor o igual
+print(num1 >= num2)
---
+++ 06-ejercicio.py
@@ -0,0 +1,10 @@
+nombre = input("Ingrese su nombre");
+edad = input("Digita tu edad")
+
+print("Hola ", nombre and edad >= 18);
+
+# True (+18)
+  print("Eres mayor de edad")
+
+# False 
+  print("Aún eres menor")
